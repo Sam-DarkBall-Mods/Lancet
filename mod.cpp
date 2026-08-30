281611fbd00e0b38534d6b6773d103bc6b843c53
Владимир <124524424+DarkBall123@users.noreply.github.com>
2026-08-30 21:15:25 +0300

    Restore Workshop mod icons (#9)

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,9 @@
 name = "Lancet 1.0.0";
+picture = "logo128.paa";
+logo = "logo128.paa";
+logoOver = "logo128.paa";
+logoSmall = "logo64.paa";
+overviewPicture = "logo128.paa";
 actionName = "GitHub";
 action = "https://github.com/Sam-DarkBall-Mods/Lancet";
 description = "Lancet - Version 1.0.0";
